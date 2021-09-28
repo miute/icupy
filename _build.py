1f41af2802d7f61dbd9b13539fd3b52db3b78c6b
Miute <miute.dev@gmail.com>
2021-09-28 21:58:50 +0900

    Use CMake environment variables

--- _build.py
+++ _build.py
@@ -1,7 +1,6 @@
 import os
 import subprocess
 import sys
-from distutils import log
 
 from setuptools import Extension
 from setuptools.command.build_ext import build_ext
@@ -31,9 +30,10 @@ class CMakeBuild(build_ext):
         if not extdir.endswith(os.path.sep):
             extdir += os.path.sep
 
-        cfg = "Debug" if self.debug else "Release"
-
         env = os.environ.copy()
+        cmake_build_type = env.get("CMAKE_BUILD_TYPE", "Release")
+        cfg = "Debug" if self.debug else cmake_build_type
+
         env["CXXFLAGS"] = " ".join([
             env.get("CXXFLAGS", "").strip(),
             "-DVERSION_INFO=\\\"{}\\\"".format(
@@ -42,8 +42,6 @@ class CMakeBuild(build_ext):
 
         cmake_args = [
             "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + extdir,
-            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}".format(
-                cfg.upper(), extdir),
             "-DPYTHON_EXECUTABLE=" + sys.executable,
             "-DCMAKE_BUILD_TYPE=" + cfg,
         ]
@@ -52,38 +50,65 @@ class CMakeBuild(build_ext):
 
         if sys.platform.startswith("win"):
             if "ICU_ROOT" not in env:
-                self.warn("ICU_ROOT variable is not defined.")
                 env["ICU_ROOT"] = "C:\\icu"
+                self.warn(
+                    "ICU_ROOT environment variable was not set. "
+                    "Using default '{}'.".format(env["ICU_ROOT"]))
             cmake_args += [
                 "-Wno-dev",
             ]
 
-        if self.compiler.compiler_type == "msvc":
+        cmake_generator = env.get("CMAKE_GENERATOR")
+        is_msbuild = (
+                self.compiler.compiler_type == "msvc"
+                and (cmake_generator is None
+                     or cmake_generator.startswith("Visual Studio"))
+        )
+        is_multi_config = (
+                is_msbuild
+                or cmake_generator in ["Ninja Multi-Config", "Xcode"]
+        )
+
+        if is_multi_config:
             cmake_args += [
-                "-A", PLAT_TO_CMAKE[self.plat_name],
+                "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}".format(
+                    cfg.upper(), extdir),
             ]
             build_args += [
                 "--config", cfg,
             ]
-        else:
-            cmake_generator = env.get("CMAKE_GENERATOR")
-            if (cmake_generator is None
-                    and "CMAKE_BUILD_PARALLEL_LEVEL" not in env):
-                build_args += [
-                    "--parallel", "4",
+
+        if is_msbuild:
+            if "CMAKE_GENERATOR_PLATFORM" not in env:
+                cmake_args += [
+                    "-A", PLAT_TO_CMAKE[self.plat_name],
                 ]
 
-        self.announce("-- CXXFLAGS variable: {}"
-                      "".format(repr(env.get("CXXFLAGS"))),
-                      level=log.INFO)
-        self.announce("-- CMAKE* variables: {}"
-                      "".format(["{}={}".format(k, v) for k, v in env.items()
-                                 if k.upper().startswith("CMAKE")]),
-                      level=log.INFO)
-        self.announce("-- CMake options: {}".format(cmake_args),
-                      level=log.INFO)
-        self.announce("-- Build options: {}".format(build_args),
-                      level=log.INFO)
+        if ("CMAKE_BUILD_PARALLEL_LEVEL" not in env
+                and not is_msbuild
+                and (cmake_generator is None
+                     or not cmake_generator.startswith("Ninja"))):
+            jobs = ((hasattr(self, "parallel") and self.parallel)
+                    or os.cpu_count())
+            if jobs:
+                build_args += [
+                    "--parallel", str(jobs),
+                ]
+
+        self.announce(
+            "-- CXXFLAGS environment variable: " + repr(env.get("CXXFLAGS")),
+            level=2)
+        self.announce(
+            "-- CMake environment variables: " + repr(
+                ["{}={}".format(k, v) for k, v in env.items()
+                 if k.upper().startswith("CMAKE")]),
+            level=2)
+        self.announce(
+            "-- CMake build system options: " + repr(cmake_args),
+            level=2)
+        self.announce(
+            "-- CMake build options: " + repr(build_args),
+            level=2)
 
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
