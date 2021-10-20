d3b6f20bec42214dd3497d74b387a3acd1fd9f8c
Miute <miute.dev@gmail.com>
2021-10-20 22:46:53 +0900

    Implement Arabic shaping API (ushape)

--- src/main.cpp
+++ src/main.cpp
@@ -105,6 +105,7 @@ void init_ures(py::module &m);
 void init_uscript(py::module &m);
 void init_usearch(py::module &m);
 void init_uset(py::module &m);
+void init_ushape(py::module &m);
 void init_ustring(py::module &m);
 void init_utext(py::module &m);
 void init_utrans(py::module &m);
@@ -280,6 +281,7 @@ PYBIND11_MODULE(MODULE_NAME, m) {
   init_uscript(m);
   init_usearch(m);
   init_uset(m);
+  init_ushape(m);
   init_ustring(m);
   init_utext(m);
   init_utrans(m);
---
+++ tests/test_ushape.py
@@ -0,0 +1,30 @@
+from icupy import (
+    U_SHAPE_DIGITS_AN2EN, U_SHAPE_DIGITS_EN2AN,
+    U_SHAPE_DIGIT_TYPE_AN, U_SHAPE_DIGIT_TYPE_AN_EXTENDED,
+    u_shape_arabic,
+)
+
+
+def test_api():
+    # From icu/source/test/cintltst/cbiditst.c
+    source = "\x31\u0627\x32\u06f3\x61\x34"
+
+    # int32_t u_shapeArabic(
+    #       const UChar *source,
+    #       int32_t sourceLength,
+    #       UChar *dest,
+    #       int32_t destSize,
+    #       uint32_t options,
+    #       UErrorCode *pErrorCode
+    # )
+    # european -> arabic
+    options = U_SHAPE_DIGITS_EN2AN | U_SHAPE_DIGIT_TYPE_AN
+    dest = u_shape_arabic(source, len(source), options)
+    assert isinstance(dest, str)
+    assert dest == "\u0661\u0627\u0662\u06f3\x61\u0664"
+
+    # arabic -> european
+    options = U_SHAPE_DIGITS_AN2EN | U_SHAPE_DIGIT_TYPE_AN_EXTENDED
+    dest = u_shape_arabic(source, -1, options)
+    assert isinstance(dest, str)
+    assert dest == "\x31\u0627\x32\x33\x61\x34"
