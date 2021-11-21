5a3802b6a58821a44ab67172e14c14acd1006d4a
miute <miute.dev@gmail.com>
2021-11-21 22:23:23 +0900

    Add DLL search path for ICU4C

--- src/icupy/__init__.py
+++ src/icupy/__init__.py
@@ -2,6 +2,18 @@
 Python bindings for ICU4C
 """
 
+import sys
+
+if sys.platform.startswith("win"):
+    import os
+    from pathlib import Path
+
+    path = (Path(os.getenv("ICU_ROOT", "C:/icu"))
+            / "bin64" if sys.maxsize > 2 ** 32 else "bin").resolve()
+    assert path.is_dir(), path  # ICU4C not found
+    os.add_dll_directory(path)
+    del path
+
 from .icu import *  # noqa
 
 __version__ = icu.VERSION_INFO  # noqa
--- tests/__init__.py
+++ tests/__init__.py
@@ -2,19 +2,10 @@ import sys
 from contextlib import contextmanager
 from pathlib import Path
 
-
-def _startup():
-    path = str((Path(__file__).parent / ".." / "src").resolve())
-    if path not in sys.path:
-        sys.path.append(path)
-
-    if sys.platform.startswith("win"):
-        import os
-
-        path = (Path(os.getenv("ICU_ROOT", "C:\\icu"))
-                / "bin64" if sys.maxsize > 2 ** 32 else "bin").resolve()
-        assert path.is_dir()  # ICU4C not found
-        os.add_dll_directory(path)
+path = str((Path(__file__).parent / ".." / "src").resolve())
+if path not in sys.path:
+    sys.path.append(path)
+del path
 
 
 @contextmanager
@@ -24,6 +15,3 @@ def gc(obj, closer):
         yield obj
     finally:
         closer(obj)
-
-
-_startup()
